603262aa6a2c8e16e8c9ed08dd7765df38849ea6
BaltiBlast <fougeray.florian@gmail.com>
2025-09-19 18:14:12 +0200

    router ok

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const router = require("./router");
 
 const PORT = 3000;
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`La broche tourne sur : http://localhost:${PORT}`);
