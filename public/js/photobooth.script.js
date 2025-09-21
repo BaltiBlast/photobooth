d114d6062a04f402db08d53331a9e0eaafbb9edc
BaltiBlast <fougeray.florian@gmail.com>
2025-09-21 15:22:47 +0200

    init stream video from camera

--- public/js/photobooth.script.js
+++ public/js/photobooth.script.js
@@ -1,7 +1,26 @@
 const photobooth = {
   init: () => {
-    console.log("photobooth script ok");
+    startLiveCamera();
+  },
+
+  startLiveCamera: async () => {
+    const videoEl = document.getElementById("video");
+
+    const constraints = {
+      video: {
+        width: { ideal: 1920 },
+        height: { ideal: 1080 },
+        aspectRatio: { ideal: 16 / 9 },
+        frameRate: { ideal: 30, max: 60 },
+      },
+      audio: false,
+    };
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    videoEl.srcObject = stream;
   },
 };
+
+const { startLiveCamera } = photobooth;
 
 document.addEventListener("DOMContentLoaded", photobooth.init());
