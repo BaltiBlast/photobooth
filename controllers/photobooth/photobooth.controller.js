0dca021043f190e7a0702ded65c16f1e6eb2eff2
BaltiBlast <fougeray.florian@gmail.com>
2025-09-22 13:01:54 +0200

    route + controller + fake picture to download on DB

--- controllers/photobooth/photobooth.controller.js
+++ controllers/photobooth/photobooth.controller.js
@@ -1,6 +1,19 @@
+const fs = require("fs");
+
 const photobooth = {
   getPhotobooth: (req, res, next) => {
     res.render("photobooth");
+  },
+
+  uploadPicture: async (req, res, next) => {
+    const picture = await fs.readFileSync("./public/assets/test.jpg");
+    try {
+      console.log("PICTURE FROM", picture);
+      return res.send("BIEN RECU");
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send("Erreur de lecture du fichier");
+    }
   },
 };
 
--- router.js
+++ router.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const photobooth = require("./controllers/photobooth/photobooth.controller");
-const { getPhotobooth } = photobooth;
+const { getPhotobooth, uploadPicture } = photobooth;
 
 router.get("/", getPhotobooth);
+router.post("/take-picture", uploadPicture);
 
 module.exports = router;
